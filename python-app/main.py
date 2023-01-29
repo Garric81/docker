1502f618bb83bd77586c8f8a5e55f9fa7babc7d5
Garric81 <50496868+Garric81@users.noreply.github.com>
2023-01-29 17:03:00 +0300

    ceate python-app

---
+++ python-app/main.py
@@ -0,0 +1,6 @@
+import calendar
+print('Добро пожаловать вк кадендарь\n')
+year = int(input("Пожалуйста введите год:"))
+month = int(input('Ведите номер  любого  месяца'))
+print(calendar.month(year,month))
+print('Всего  хорошего')
