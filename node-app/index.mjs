c8564b0b3193b11b6d7d53ef4fec09d9a3d11228
Garric81 <50496868+Garric81@users.noreply.github.com>
2023-02-04 20:08:33 +0300

    create dcocker compose -app

--- node-app/index.mjs
+++ node-app/index.mjs
@@ -2,5 +2,7 @@ import fs 'fs'
 
 fs.appendFile('my-File.txt', 'файл создан Node.js', (err) => {
     if (err) throw err
-    console.log('файл сохранен')
+    console.log('файл сохранен!')
 })
+
+setTimeout(() => console.log ("Завершение"), 20000)
